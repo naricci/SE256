3c09450e8f5075f817dc434a738b0264b7d29164
Nicholas Ricci <naricci@email.neit.edu>
2017-04-20 11:00:30 -0400

    rename Default to ContactMgr, create new pages/page links in nav

--- Lab2_MasterPages/Default.aspx.cs
+++ Lab2_MasterPages/Default.aspx.cs
@@ -7,70 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-
     protected void Page_Load(object sender, EventArgs e)
     {
 
-    }
-
-    protected void btnAdd_Click(object sender, EventArgs e)
-    {
-        // CheckBox Text Logic
-        if (chkCardWorthy.Checked)
-        {
-            chkCardWorthy.Text = "Card Worthy";
-        }
-        else
-        {
-            chkCardWorthy.Text = "Not Card Worthy";
-        }
-
-        // Add Contact Name to Feedback Label
-        lblFeedback.Text = txtFName.Text + " " + txtLName.Text;
-
-        // Add Form Data to ListBox
-        lbxFeedback.Items.Add(txtFName.Text + " " + txtLName.Text);
-        lbxFeedback.Items.Add(txtStreet1.Text + " " + txtStreet2.Text);
-        lbxFeedback.Items.Add(txtCity.Text + ", " + ddlState.SelectedItem.ToString() + ", " + txtZipCode.Text);
-        lbxFeedback.Items.Add(calBirthday.SelectedDate.ToString());
-        lbxFeedback.Items.Add(calAnniversary.SelectedDate.ToString());
-        lbxFeedback.Items.Add(txtHomePhone.Text);
-        lbxFeedback.Items.Add(txtWorkPhone.Text);
-        lbxFeedback.Items.Add(txtCellPhone.Text);
-        lbxFeedback.Items.Add(chkCardWorthy.Text.ToString());
-        lbxFeedback.Items.Add(ddlRelationship.SelectedItem.ToString());
-        lbxFeedback.Items.Add(txtNotes.Text);
-    }
-
-    protected void btnClear_Click(object sender, EventArgs e)
-    {
-        // Clear the TextBoxes
-        txtFName.Text = "";
-        txtLName.Text = "";
-        txtStreet1.Text = "";
-        txtStreet2.Text = "";
-        txtCity.Text = "";
-        txtZipCode.Text = "";
-        txtHomePhone.Text = "";
-        txtWorkPhone.Text = "";
-        txtCellPhone.Text = "";
-        txtNotes.Text = "";
-
-        // Clear the Drop-Down Lists
-        ddlState.SelectedIndex = 0;
-        ddlRelationship.SelectedIndex = 0;
-
-        // Clear the Calendars
-        calBirthday.SelectedDates.Clear();
-        calAnniversary.SelectedDates.Clear();
-
-        // Clear the CheckBoxes
-        chkCardWorthy.Checked = false;
-
-        // Clear Feedback Label
-        lblFeedback.Text = "Feedback";
-
-        // Clear Feedback ListBox
-        lbxFeedback.Items.Clear();
     }
 }
